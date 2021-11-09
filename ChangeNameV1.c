d42aa93f03ed00ef8fc9a75895396fa858deeacc
denisplay <workingdenniss@gmail.com>
2021-11-09 19:41:04 +0200

    Consept of ChangeNameV1

---
+++ ChangeNameV1.c
@@ -0,0 +1,14 @@
+#include "includeAll.h"
+
+void ChangeNameV1(char *argv[])
+{
+    //        argv[0]  argv[1]    argv[2]                        argv[3]
+    //format --> .exe 1          0/1                       "path1,path2,path3..."
+    //                ^          ^                                   ^
+    //              this         Function to                place where the files
+    //              Function     GetNumbersOutOfString      are to be renamed  
+    //                           (V2 is default 1)
+
+    
+
+}
--- main.c
+++ main.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
             Description();
             break;
         case ChangeV1:
-            ChangeNameV1(argv)
+            ChangeNameV1(argv);
             break;
         default:
             break;
